d1e73059c01c4eae869f9f97c7273b7f50a0befc
arkandria <miguelfariasgp@gmail.com>
2022-07-26 13:14:58 -0500

    add keyboard functionality

--- javascript.js
+++ javascript.js
@@ -51,12 +51,19 @@ let histArray = [];
 let display;
 let dotCounter=0;
 
-const totalDisplay = document.querySelector(".total")
+const totalDisplay = document.querySelector(".total");
 
 insertN = (n) => {
   numberArr.push(n);
   totalDisplay.textContent = numberArr.join('');
 };
+
+insertZero = () => {
+  if (!((numberArr.length===0)|| (numberArr[numberArr.length-1]==' + '||numberArr[numberArr.length-1]==' - ' ||numberArr[numberArr.length-1]==' x ' ||numberArr[numberArr.length-1]==' ÷ '))) {
+    insertN(0);
+  } 
+};
+
 insertDot = () => {
   if (dotCounter===0){
     numberArr.push(".");
@@ -64,11 +71,17 @@ insertDot = () => {
     dotCounter++;
 }
 };
+
 deleteAll = () => {
   numberArr = [] ;
   operations = [] ;
   totalDisplay.textContent = 0;
+  a = null;
+  b = null;
+  op = null;
+  total = 0;
 };
+
 deleteOne = () => {
   numberArr.pop();
   if (numberArr.length===0){
@@ -116,11 +129,7 @@ nine.addEventListener ('click', () => {
 });
 const zero = document.querySelector("#zero");
 zero.addEventListener ('click', () => {
-  
-  if (!((numberArr.length===0)|| (numberArr[numberArr.length-1]==' + '||numberArr[numberArr.length-1]==' - ' ||numberArr[numberArr.length-1]==' x ' ||numberArr[numberArr.length-1]==' ÷ '))) {
-    insertN(0);
-  } 
-  
+  insertZero();  
 });
 const dot = document.querySelector("#dot");
 dot.addEventListener ('click', () => {
@@ -142,58 +151,133 @@ del.addEventListener ('click', () => {
   deleteOne();
 });
 
+window.addEventListener ('keydown', (event) => {
+  console.log(`key=${event.key},code=${event.code}`);
+  if (event.key==="1"){
+    insertN(1);
+  };
+  if (event.key==="2"){
+    insertN(2);
+  };
+  if (event.key==="3"){
+    insertN(3);
+  };
+  if (event.key==="4"){
+    insertN(4);
+  };
+  if (event.key==="5"){
+    insertN(5);
+  };
+  if (event.key==="6"){
+    insertN(6);
+  };
+  if (event.key==="7"){
+    insertN(7);
+  };
+  if (event.key==="8"){
+    insertN(8);
+  };
+  if (event.key==="9"){
+    insertN(9);
+  };
+  if (event.key==="0"){
+    insertN(0);
+  };
+  if (event.key==="."){
+    insertDot();
+  };
+  if (event.key==="Backspace"){
+    deleteOne();
+  };
+  if (event.key==="+"){
+    if (numberArr.length > 0) {
+      addOperator(" + ");
+    }
+  };
+  if (event.key==="-"){
+    if (numberArr.length===0){
+      numberArr.push(" - ");
+      totalDisplay.textContent = numberArr.join('');
+    } else {
+      addOperator(" - ");
+    }
+  };
+  if (event.key==="/"){
+    if (numberArr.length > 0) {
+      addOperator(" ÷ ");
+    };
+  };
+  if (event.key==="*"){
+    if (numberArr.length > 0) {
+      addOperator(" x ");
+    }
+  };
+  if (event.key==="="){
+    operationsHandler();
+  };
+  if (event.key==="Enter"){
+    operationsHandler();
+  };
+});
+
+
+
+let checkLastEl = () => {
+  if (numberArr[numberArr.length-1]=== " + " || numberArr[numberArr.length-1]=== " - " || numberArr[numberArr.length-1]=== " x " || numberArr[numberArr.length-1]===" ÷ ") {
+    return true;
+  }
+};
+
+let addOperator = (opString) => {
+  if (checkLastEl())  {
+    numberArr.pop();
+  };
+  numberArr.push(opString);
+  totalDisplay.textContent = numberArr.join('');
+}
+
+
+
 const plus = document.querySelector("#add");
 plus.addEventListener ('click', () => {
-  
-  if (numberArr[numberArr.length-1]=== "+" || numberArr[numberArr.length-1]=== "-" || numberArr[numberArr.length-1]=== "x" || numberArr[numberArr.length-1]==="÷") {
-    numberArr.pop();
+ if (numberArr.length > 0) {
+    addOperator(" + ");
   }
-  numberArr.push(" + ");
-  totalDisplay.textContent = numberArr.join('');
-  });
+});
 
 const subst = document.querySelector("#subs");
   subst.addEventListener ('click', () => {
-    if (numberArr[numberArr.length-1]=== "+" || numberArr[numberArr.length-1]=== "-" || numberArr[numberArr.length-1]=== "x" || numberArr[numberArr.length-1]==="÷") {
-      numberArr.pop();
+    if (numberArr.length===0){
+      numberArr.push(" - ");
+      totalDisplay.textContent = numberArr.join('');
+    } else {
+      addOperator(" - ");
     }
-    numberArr.push(" - ");
-    totalDisplay.textContent = numberArr.join('');
+    
     });
 
 const multi = document.querySelector("#mult");
   multi.addEventListener ('click', () => {
-    if (numberArr[numberArr.length-1]=== "+" || numberArr[numberArr.length-1]=== "-" || numberArr[numberArr.length-1]=== "x" || numberArr[numberArr.length-1]==="÷") {
-      numberArr.pop();
+    if (numberArr.length > 0) {
+      addOperator(" x ");
     }
-    
-    numberArr.push(" x ");
-    totalDisplay.textContent = numberArr.join('');
     });
-
+    
 const division = document.querySelector("#divide");
 division.addEventListener ('click', () => {
-  
-  if (numberArr[numberArr.length-1]=== "+" || numberArr[numberArr.length-1]=== "-" || numberArr[numberArr.length-1]=== "x" || numberArr[numberArr.length-1]==="÷") {
-    numberArr.pop();
-  }
-  numberArr.push(" ÷ ");
-  
-  totalDisplay.textContent = numberArr.join('');
+  if (numberArr.length > 0) {
+    addOperator(" ÷ ");
+  };
   });
 
 const equals = document.querySelector("#equals");
-equals.addEventListener ('click', () => {
-    
-    a = null;
-    b = null;
-    op = null;
-    operations =[];
-    if (numberArr[numberArr.length-1]=== "+" || numberArr[numberArr.length-1]=== "-" || numberArr[numberArr.length-1]=== "x" || numberArr[numberArr.length-1]==="÷") {
-      numberArr.pop();
-    };
-  let temp = []; 
-  numberArr.push(" = ");
+  equals.addEventListener ('click', () => {
+    operationsHandler();
+  });
+
+
+let operationsBuilder = () => {
+  let temp =[];
   for (i=0; i<numberArr.length; i++) {
     if (typeof numberArr[i]==='number') {
       temp.push(numberArr[i]);
@@ -203,50 +287,50 @@ equals.addEventListener ('click', () => {
       operations.push(numberArr[i]);
     }
   }
-  
-  //console.log(operations);
-  
+};
+
+let fixNan = () => {
   if (Number.isNaN(operations[0])) {
     operations.shift();
     
   };
-  
-  if (operations[0]== ' - ' || operations[0]===' + '|| operations[0]==' x '|| operations[0]===' ÷ ') {
-    
-    operations.unshift(total);
-    
-  };
+};
 
+let operationsHandler = () => {
+  a = null;
+  b = null;
+  op = null;
+  operations =[];
+  
+  if (checkLastEl()) {
+    numberArr.pop();
+  };
+    
+  numberArr.push(" = ");
+  
+  operationsBuilder();
+  fixNan();
+    
+  if (operations[0]== ' - ' ) {
+    operations.unshift(total);
+   };
+  
   for (i=0; i<operations.length; i++) {
-   
     if (i===0 && typeof operations[i]==='number') {
-      a = operations[i];
-      
+      a = operations[i]; 
     } else if (i>1 && typeof operations[i]==='number') {
-     
-      b = operations[i];
-      
-      
+      b = operations[i];      
     } else if (i>0 && (operations[i]===' + '|| operations[i]===' - '|| operations[i]==' x '|| operations[i]===' ÷ ')) {
       if (op== null){
         op= operations[i];
-      
-        
       } else   {
-        
         total = operate(a,op,b);
-        
         a= total;
-        
         b= null;
         op=operations[i];
-        
-      }
-    
-      
+      };  
     } else if (operations[i]===' = ') {
       if (!(op== null && b== null)){
-        
         total = operate(a,op,b);
         totalDisplay.textContent = total;
         b= null;
@@ -257,5 +341,7 @@ equals.addEventListener ('click', () => {
       }      
     } 
   };
-});
+};
+
+
 
